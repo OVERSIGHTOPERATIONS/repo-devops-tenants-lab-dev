447413a6a921a2547f556b45003b98e5a3cd149a
David Dellanno <david.dellanno@oversightoperations.com>
2025-02-02 20:19:06 -0500

    update create private dns zone script

---
+++ scripts/create_private_dns_zone.sh
@@ -0,0 +1,20 @@
+# Description: Create a private DNS zone in Azure
+az network vnet create \
+  --name myAzureVNet \
+  --resource-group MyAzureResourceGroup \
+  --location eastus \
+  --address-prefix 10.2.0.0/16 \
+  --subnet-name backendSubnet \
+  --subnet-prefixes 10.2.0.0/24
+
+az network private-dns zone create -g MyAzureResourceGroup \
+   -n private.contoso.com
+
+az network private-dns link vnet create -g MyAzureResourceGroup -n MyDNSLink \
+   -z private.contoso.com -v myAzureVNet -e true
+
+az network private-dns zone list -g MyAzureResourceGroup
+
+az network private-dns zone list -g MyAzureResourceGroup \
+   --query "[].{Name:name, ResourceGroup:resourceGroup, ProvisioningState:provisioningState}" \
+   --output table
